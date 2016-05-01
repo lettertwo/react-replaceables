b138136b72d7437ab040e83814d522500ccf7631
Eric Eldredge <lettertwo@gmail.com>
2016-05-01 15:28:21 -0400

    Add devDeps and jsdom bootstrapping for enzyme

--- mocha-bootstrap.js
+++ mocha-bootstrap.js
@@ -9,3 +9,15 @@ require('babel-register')({
     'babel-plugin-espower',
   ],
 });
+
+// Create a browser-like environment, if necessary.
+if (!global.window) {
+  global.document = require('jsdom').jsdom('<!doctype html><html><body></body></html>');
+  global.window = document.defaultView;
+
+  for (let key in global.window) {
+    if (!global.window.hasOwnProperty(key)) continue;
+    if (key in global) continue;
+    global[key] = global.window[key];
+  }
+}
